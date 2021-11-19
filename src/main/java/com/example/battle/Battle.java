0a991a8bc092f1c27ea448a9bc18967dc2ec54dd
koufu193 <73023297+koufu193@users.noreply.github.com>
2021-11-19 20:29:45 +0900

    しなければいけないことまとめた

--- src/main/java/com/example/battle/Battle.java
+++ src/main/java/com/example/battle/Battle.java
@@ -1,135 +1,34 @@
 package com.example.battle;
 
-import org.bukkit.*;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandSender;
-import org.bukkit.configuration.Configuration;
-import org.bukkit.configuration.file.FileConfiguration;
-import org.bukkit.entity.Player;
-import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.scheduler.BukkitRunnable;
-import org.bukkit.scoreboard.Objective;
-import org.bukkit.scoreboard.Scoreboard;
 
-import java.io.File;
-import java.util.*;
-
-public final class Battle extends JavaPlugin {
-    Map<String, ChatColor> playerData=new HashMap<>();
-    util util;
-    int[] number=new int[2];//1:RED,2:BLUE
-    Random random=new Random();
-    Scoreboard scoreboard;
-    boolean isStop=false;
-    Location[] locations=new Location[3];//ダイヤ,鉄,金の順番
-    FileConfiguration config;
-    ArrayList<Player> redteam = new ArrayList<>();
-    ArrayList<Player> blueteam = new ArrayList<>();
-    ArrayList<Player> noteam = new ArrayList<>();
-    Location diamond;
-    Location iron;
-    Location gold;
-
-    Location redSpawn;
-    Location blueSpawn;
+public class Battle extends JavaPlugin {
+    util util=new util(this);
+    final String KISHI_AKASHI_NAME="騎士の証";
+    final String SAKIMORI_AKASHI_NAME="防人の証";
     @Override
     public void onEnable() {
-        isStop=false;
         saveDefaultConfig();
-        config = getConfig();
-        Bukkit.getPluginManager().registerEvents(new event(this),this);
-        util=new util(this);
-        scoreboard = Bukkit.getScoreboardManager().getNewScoreboard();
-        scoreboard.registerNewTeam("red").setPrefix("§c");
-        scoreboard.registerNewTeam("blue").setPrefix("§9");
-        scoreboard.getTeam("red").setAllowFriendlyFire(false);
-        scoreboard.getTeam("blue").setAllowFriendlyFire(false);
-        diamond = util.getlocationbyconfig(config,"locations.diamond",Bukkit.getWorld("world"));
-        iron = util.getlocationbyconfig(config,"locations.iron",Bukkit.getWorld("world"));
-        gold = util.getlocationbyconfig(config,"locations.gold",Bukkit.getWorld("world"));
-        blueSpawn=util.getlocationbyconfig(config,"locations.blueLocation",Bukkit.getWorld("world"));
-        redSpawn=util.getlocationbyconfig(config,"locations.redLocation",Bukkit.getWorld("world"));
-        for (String name:config.getStringList("players.red")) {
-            redteam.add((Player) Bukkit.getOfflinePlayer(UUID.fromString(name)));
-        }
-        for (String name:config.getStringList("players.blue")) {
-            blueteam.add((Player) Bukkit.getOfflinePlayer(UUID.fromString(name)));
-        }
-        for (Player p:Bukkit.getOnlinePlayers()) {
-            if (blueteam.contains(p)) {
-                scoreboard.getTeam("blue").addEntry(p.getName());
-            } else if (redteam.contains(p)) {
-                scoreboard.getTeam("red").addEntry(p.getName());
-            }
-        }
-        for (Player p:Bukkit.getOnlinePlayers()) {
-            p.setScoreboard(scoreboard);
-        }
-        CheckExile();
-        new BukkitRunnable(){
-            @Override
-            public void run() {
-                int diamond=getConfig().getInt("diamond");
-                int iron=getConfig().getInt("iron");
-                int gold=getConfig().getInt("gold");
-                int[] data=new int[3];
-                while(!isStop){
-                    for(int number:data){
-                        number++;
-                    }
-                    if(diamond<=data[0]){
-                        data[0]=0;
-
-                    }
-                    if(iron<=data[1]){
-                        data[1]=0;
-                    }
-                    if(gold<=data[2]){
-                        data[2]=0;
-                    }
-                    try {
-                        Thread.sleep(1000);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-        }.runTask(this);
     }
     @Override
     public void onDisable() {
-        ArrayList<String> blueuuid = new ArrayList<>();
-        ArrayList<String> reduuid = new ArrayList<>();
-        blueteam.forEach(player -> {
-            blueuuid.add(player.getUniqueId().toString());
-        });
-        redteam.forEach(player -> {
-            reduuid.add(player.getUniqueId().toString());
-        });
-        config.set("players.blue",blueuuid);
-        config.set("players.red",reduuid);
         saveConfig();
-        isStop=true;
     }
-
-    private void CheckExile() {
-        for (Player p:Bukkit.getOnlinePlayers()) {
-            if (!p.getInventory().containsAtLeast(new ItemStack(Material.DIAMOND),1)&&noteam.contains(p)){
-                noteam.remove(p);
-                scoreboard.getTeam(playerData.get(p.getName()).name()).addEntry(p.getName());
-                p.setHealth(0);
-                continue;
-            }
-            if (!noteam.contains(p)) {
-                noteam.add(p);
-                scoreboard.getTeam(playerData.get(p.getName()).name()).removeEntry(p.getName());
-                p.getServer().broadcast("a",p.getName()+"が亡命の心を手に入れました");
-            }
-        }
-        for (Player p:Bukkit.getOnlinePlayers()) {
-            p.setScoreboard(scoreboard);
-        }
-        Bukkit.getScheduler().scheduleSyncDelayedTask(this,this::CheckExile,5*20L);
-    }
+    //-4000 65 0 <-blue spawn
+    //4000 65 0 <-red spawn
+    //20x±10x20よりうえ
+    //30×±15x30以下
+    //に旗おける
+    //騎士の証や防人の証＜＝dark red or dark blue
+    //騎士:攻撃力増加レベル3
+    //防人:耐性レベル3
+    //アプサラスへの亡命書(青)<-dark blue
+    //アルティオへの亡命書(赤)<-dark red
+    //アプサラス制圧旗(青色の旗)<-dark blue
+    //アルティオ制圧旗(赤色の旗),-dark red
+    //おかれたら旗は1時間ごとにアナウンス、24時間経過で終了
+    //()は除く
+    //制圧を示す制圧旗が立てられ○○時間が経過しました
+    //無所属の場合：その人が持っている証は消す
+    //捨てたら消える(どのチームでも)
 }
--- src/main/java/com/example/battle/util.java
+++ src/main/java/com/example/battle/util.java
@@ -12,16 +12,12 @@ public class util {
     public util(Battle battle){
         this.battle=battle;
     }
-    public void makeNewWorld(){
-        if(Bukkit.getWorld(this.battle.getConfig().getString("newWorldName"))!=null){
-            Bukkit.unloadWorld(this.battle.getConfig().getString("newWorldName"),true);
-            deleteWorld(Bukkit.getWorld(this.battle.getConfig().getString("newWorldName")).getWorldFolder());
+    public void makeNewWorld(String worldName){
+        if(Bukkit.getWorld(worldName)!=null){
+            Bukkit.unloadWorld(worldName,true);
+            deleteWorld(Bukkit.getWorld(worldName).getWorldFolder());
         }
-        Bukkit.createWorld(new WorldCreator(this.battle.getConfig().getString("newWorldName")).type(WorldType.NORMAL));
-        String[] data={"diamond","iron","gold"};
-        for(int i=0;i<3;i++){
-            this.battle.locations[i]=new Location(Bukkit.getWorld(this.battle.getConfig().getString("newWorldName")),this.battle.getConfig().getInt("Locations."+data[i]+".x"),this.battle.getConfig().getInt("Locations."+data[i]+".y"),this.battle.getConfig().getInt("Locations."+data[i]+".z"));
-        }
+        Bukkit.createWorld(new WorldCreator(worldName).type(WorldType.NORMAL));
     }
     public void deleteWorld(File f){
         if(f.isDirectory()){
@@ -30,24 +26,5 @@ public class util {
             }
         }
         f.delete();
-    }
-    public void addPlayer(Player p, ChatColor color){
-        p.setBedSpawnLocation(color==ChatColor.BLUE?battle.blueSpawn:battle.redSpawn);
-        p.setScoreboard(battle.scoreboard);
-        this.battle.playerData.put(p.getUniqueId().toString(),color);
-        p.setPlayerListName(color+p.getPlayerListName()+ChatColor.RESET);
-        p.setDisplayName(color+p.getDisplayName()+ChatColor.RESET);
-        this.battle.scoreboard.getTeam(color==ChatColor.RED?"red":"blue").addPlayer(p);
-        if (color==ChatColor.RED) {
-            battle.redteam.add(p);
-        } else if (color==ChatColor.BLUE){
-            battle.blueteam.add(p);
-        }
-    }
-    public Location getlocationbyconfig(FileConfiguration config, String path, World world) {
-        double x = config.getDouble(path+".x");
-        double y = config.getDouble(path+".y");
-        double z = config.getDouble(path+".z");
-        return new Location(world,x,y,z);
     }
 }
